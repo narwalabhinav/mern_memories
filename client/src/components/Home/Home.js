7ceb7272e5614bb4d3086b2f77104c087be65f7c
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-09-08 10:33:33 +0530

    Removed unnecssary imports

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {  useState, useEffect } from 'react';
+import React, {  useState } from 'react';
 import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -2,15 +2,14 @@ import React, { useEffect } from 'react';
 import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { getPost } from '../../actions/posts';
 import useStyles from './styles';
 
 const PostDetails = () => {
-  const { post, posts, isLoading } = useSelector((state) => state.posts);
+  const { post, isLoading } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
-  const history = useHistory();
   const classes = useStyles();
   const { id } = useParams();
 
