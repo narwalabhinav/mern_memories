2e1f8b48b51d76d68e6ccc4b61c10371f4609527
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-09-08 11:32:12 +0530

    build

---
+++ client/build/precache-manifest.46dae7122b4ead4bc08a43f03a175be7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c4a6819232ddea5d15c1aaa5364ed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e316cc20fdc1905ca54",
+    "url": "/static/css/main.b903f5df.chunk.css"
+  },
+  {
+    "revision": "09ec3fb2b239175ef8ff",
+    "url": "/static/js/2.6f480292.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.6f480292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e316cc20fdc1905ca54",
+    "url": "/static/js/main.114112d3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.114112d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "31047bc105c44ca93ee9d81b54fb196e",
+    "url": "/static/media/memories.31047bc1.jpeg"
+  }
+]);
