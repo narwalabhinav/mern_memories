f0e0f9721a5a134958f3f96efc4c48ae71eb2760
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-09-09 20:38:54 +0530

    updated cors in server/index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
